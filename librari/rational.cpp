ed1b45af88302cd0f6c56dee18b667662681f190
VOEN1TEX <adylkhanovt@bk.ru>
2023-03-19 21:29:14 +0500

    add rational

---
+++ librari/rational.cpp
@@ -0,0 +1,230 @@
+#include "rational.h"
+
+int GCD(int a, int b) {
+  while (a > 0 && b > 0) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return (a > b ? a : b);
+}
+
+void Rational::Reduce() {
+  if (q_ == 0) {
+    throw RationalDivisionByZero();
+  }
+
+  if (q_ < 0) {
+    q_ = -q_;
+    p_ = -p_;
+  }
+
+  int sign = (p_ > 0 ? 1 : (p_ = -p_, -1));  //при необходимости меняем числител,
+  int gcd = GCD(p_, q_);
+  p_ /= gcd;
+  p_ *= sign;
+  q_ /= gcd;
+}
+
+Rational::Rational(int p, int q) {
+  p_ = p;
+  q_ = q;
+
+  Reduce();
+}
+
+Rational::Rational(const Rational& r) {
+  p_ = r.p_;
+  SetDenominator(r.q_);
+}
+
+Rational::Rational() {
+  p_ = 0;
+  q_ = 1;
+}
+
+Rational::Rational(int val) {
+  p_ = val;
+}
+
+int Rational::GetNumerator() const {
+  return p_;
+}
+
+int Rational::GetDenominator() const {
+  return q_;
+}
+
+void Rational::SetNumerator(int numerator) {
+  p_ = numerator;
+  Reduce();
+}
+
+void Rational::SetDenominator(int denominator) {
+  q_ = denominator;
+  Reduce();
+}
+
+Rational Rational::operator-() const {
+  Rational copy = *this;
+  copy.p_ = -copy.p_;
+  return copy;
+}
+
+Rational Rational::operator+() const {
+  return *this;
+}
+
+Rational& Rational::operator+=(const Rational& first) {
+  p_ = p_ * first.q_ + first.p_ * q_;
+  q_ = q_ * first.q_;
+
+  Reduce();
+
+  return *this;
+}
+
+Rational& Rational::operator-=(const Rational& first) {
+  p_ = p_ * first.q_ - first.p_ * q_;
+  q_ = q_ * first.q_;
+
+  Reduce();
+
+  return *this;
+}
+
+Rational& Rational::operator*=(const Rational& first) {
+  p_ = p_ * first.p_;
+  q_ = q_ * first.q_;
+
+  Reduce();
+
+  return *this;
+}
+
+Rational& Rational::operator/=(const Rational& first) {
+  p_ = p_ * first.q_;
+  q_ = q_ * first.p_;
+
+  Reduce();
+
+  return *this;
+}
+
+Rational& Rational::operator++() {
+  *this += 1;
+  return *this;
+}
+
+Rational Rational::operator++(int) {
+  Rational copy = *this;
+  ++*this;
+  return copy;
+}
+
+Rational& Rational::operator--() {
+  *this -= 1;
+  return *this;
+}
+
+Rational Rational::operator--(int) {
+  Rational copy = *this;
+  --*this;
+  return copy;
+}
+
+std::ostream& operator<<(std::ostream& output, const Rational& val) {
+  if (val.GetDenominator() != 1) {
+    output << val.GetNumerator() << "/" << val.GetDenominator();
+  } else {
+    output << val.GetNumerator();
+  }
+  return output;
+}
+
+std::istream& operator>>(std::istream& input, Rational& val) {
+  val = 0;
+
+  char string[50];
+  input >> string;
+
+  int i = 0;
+  int sign = 1;
+  while (string[i] != 0 && string[i] != '/') {
+    if (string[i] == '+') {
+    } else if (string[i] == '-') {
+      sign *= -1;
+    } else {
+      val.SetNumerator(val.GetNumerator() * 10 + (string[i] - '0'));
+    }
+    ++i;
+  }
+
+  int denominator = 0;
+  bool denominator_exists = false;
+
+  while (string[i] != 0) {
+    if (string[i] == '/' || string[i] == '+') {
+    } else if (string[i] == '-') {
+      sign *= -1;
+    } else {
+      denominator_exists = true;
+      denominator = denominator * 10 + (string[i] - '0');
+    }
+    ++i;
+  }
+
+  val.SetNumerator(val.GetNumerator() * sign);
+  val.SetDenominator(denominator == 0 ? (denominator_exists ? 0 : 1) : denominator);
+  return input;
+}
+
+Rational operator+(const Rational& first, const Rational& other) {
+  Rational copy = first;
+  copy += other;
+  return copy;
+}
+
+Rational operator-(const Rational& first, const Rational& other) {
+  Rational copy = first;
+  copy -= other;
+  return copy;
+}
+
+Rational operator*(const Rational& first, const Rational& other) {
+  Rational copy = first;
+  copy *= other;
+  return copy;
+}
+
+Rational operator/(const Rational& first, const Rational& other) {
+  Rational copy = first;
+  copy /= other;
+  return copy;
+}
+
+bool operator>(const Rational& first, const Rational& other) {
+  return (first.GetNumerator() * other.GetDenominator() > first.GetDenominator() * other.GetNumerator());
+}
+
+bool operator<(const Rational& first, const Rational& other) {
+  return other > first;
+}
+
+bool operator==(const Rational& first, const Rational& other) {
+  return (first.GetNumerator() * other.GetDenominator() == first.GetDenominator() * other.GetNumerator());
+}
+
+bool operator>=(const Rational& first, const Rational& other) {
+  return (first > other || first == other);
+}
+
+bool operator<=(const Rational& first, const Rational& other) {
+  return (first < other || first == other);
+}
+
+bool operator!=(const Rational& first, const Rational& other) {
+  return !(first == other);
+}
