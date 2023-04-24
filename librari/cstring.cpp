53de5d35f0a46d21d3683537c6d5c4388d118d40
VOEN1TEX <adylkhanovt@bk.ru>
2023-04-24 13:29:13 +0500

    fix cstring v1

--- librari/cstring.cpp
+++ librari/cstring.cpp
@@ -3,10 +3,12 @@
 std::size_t Strlen(const char* str) {
   std::size_t str_len = 0;
   const char* curr_str = str;
+
   while (*curr_str != 0) {
     ++str_len;
     ++curr_str;
   }
+
   return str_len;
 }
 
@@ -15,6 +17,7 @@ int Strcmp(const char* first, const char* second) {
     ++first;
     ++second;
   }
+
   return *first - *second;
 }
 
@@ -29,61 +32,73 @@ int Strncmp(const char* first, const char* second, std::size_t count) {
 
 char* Strcpy(char* dest, const char* src) {
   char* curr_dest = dest;
+
   while (*src != 0) {
     *curr_dest = *src;
     ++curr_dest;
     ++src;
   }
+
   *curr_dest = '\0';
   return dest;
 }
 
 char* Strncpy(char* dest, const char* src, std::size_t count) {
   char* curr_dest = dest;
+
   while (*src != 0 && count > 0) {
     *curr_dest = *src;
     ++curr_dest;
     ++src;
     --count;
   }
+
   while (count > 0) {
     *curr_dest = '\0';
     ++curr_dest;
     --count;
   }
+
   return dest;
 }
 
 char* Strcat(char* dest, const char* src) {
   char* curr_dest = dest;
+  
   while (*curr_dest != 0) {
     ++curr_dest;
   }
+
   while (*src != 0) {
     *curr_dest = *src;
     ++curr_dest;
     ++src;
   }
+
   *curr_dest = '\0';
   return dest;
 }
 
 char* Strncat(char* dest, const char* src, std::size_t count) {
   char* curr_dest = dest;
+
   while (*curr_dest != 0) {
     ++curr_dest;
   }
+
   while (*src != 0 && count != 0) {
     *curr_dest = *src;
     ++curr_dest;
     ++src;
     --count;
   }
+
   while (count > 0) {
     *curr_dest = '\0';
     ++curr_dest;
     --count;
   }
+
   return dest;
 }
 
@@ -91,33 +106,42 @@ const char* Strchr(const char* str, char symbol) {
   while (*str != 0 && (*str != symbol)) {
     ++str;
   }
+
   return (*str == symbol ? str : nullptr);
 }
 
 const char* Strrchr(const char* str, char symbol) {
   const char* last = nullptr;
+
   while (*str != 0) {
     if (*str == symbol) {
       last = str;
     }
+
     ++str;
   }
+
   return (symbol == '\0' ? str : last);
 }
 
 std::size_t Strspn(const char* dest, const char* src) {
   std::size_t amount = 0;
+
   while (*dest != 0) {
     const char* curr_src = src;
+
     while (*curr_src != 0 && *dest != *curr_src) {
       ++curr_src;
     }
+
     if (*curr_src == 0) {
       return amount;
     }
+
     ++amount;
     ++dest;
   }
+
   return amount;
 }
 
@@ -125,33 +149,41 @@ std::size_t Strcspn(const char* dest, const char* src) {
   const char* curr_dest = dest;
   const char* curr_src = src;
   std::size_t amount = 0;
+
   while (*curr_dest != 0) {
     while (*curr_src != 0 && *curr_dest != *curr_src) {
       ++curr_src;
     }
+
     if (*curr_src != 0) {
       return amount;
     }
+
     ++amount;
     curr_src = src;
     ++curr_dest;
   }
+
   return amount;
 }
 
 const char* Strpbrk(const char* dest, const char* breakset) {
   const char* curr_dest = dest;
   const char* curr_breakset = breakset;
+
   while (*curr_dest != 0) {
     while (*curr_breakset != 0 && *curr_dest != *curr_breakset) {
       ++curr_breakset;
     }
+
     if (*curr_breakset != 0) {
       return curr_dest;
     }
+
     curr_breakset = breakset;
     ++curr_dest;
   }
+
   return nullptr;
 }
 
@@ -159,34 +191,37 @@ const char* Strstr(const char* str, const char* pattern) {
   if (Strlen(pattern) == 0) {
     return str;
   }
+
   if (Strlen(str) < Strlen(pattern)) {
     return nullptr;
   }
+
   while (*str != 0) {
     if (*str == *pattern) {
       const char* curr_str = str;
       const char* curr_pattern = pattern;
+
       while (*curr_str != 0) {
         if (*curr_pattern == 0) {
           return str;
         }
+
         if (*curr_str != *curr_pattern) {
           break;
         }
+
         ++curr_pattern;
         ++curr_str;
       }
+
       if (*curr_pattern == 0) {
         return str;
       }
+
     }
+ 
     ++str;
   }
+ 
   return nullptr;
 }
-
-int main() {
-  char s[10] = "\0";
-  std::cout << Strchr(s, '\0') - s;
-  return 0;
-} 
--- librari/cstring.h
+++ librari/cstring.h
@@ -2,28 +2,21 @@
 #define CSTRING_H
 #include <iostream>
 
-class 
-
 std::size_t Strlen(const char* str);
 
 int Strcmp(const char* first, const char* second);
-
 int Strncmp(const char* first, const char* second, std::size_t count);
 
 char* Strcpy(char* dest, const char* src);
-
 char* Strncpy(char* dest, const char* src, std::size_t count);
 
 char* Strcat(char* dest, const char* src);
-
 char* Strncat(char* dest, const char* src, std::size_t count);
 
 const char* Strchr(const char* str, char symbol);
-
 const char* Strrchr(const char* str, char symbol);
 
 std::size_t Strspn(const char* dest, const char* src);
-
 std::size_t Strcspn(const char* dest, const char* src);
 
 const char* Strpbrk(const char* dest, const char* breakset);
